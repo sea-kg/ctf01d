548388c6c2d6b90f9530bfdbd660f69413cb4b19
Evgenii Sopov <mrseakg@gmail.com>
2021-06-03 09:27:36 +0700

    configure-openvpn-server: start redesvelop newClient

--- misc/openvpn/configure-openvpn-server.py
+++ misc/openvpn/configure-openvpn-server.py
@@ -5,6 +5,7 @@ import os
 import sys
 import subprocess
 import shutil
+import re
 
 OS = ''
 VERSION_ID = ''
@@ -1077,110 +1078,10 @@ verb 3" >>/etc/openvpn/client-template.txt
     newClient
     echo "If you want to add more clients, you simply need to run this script another time!"
 }
-
-function newClient() {
-    echo ""
-    echo "Tell me a name for the client."
-    echo "The name must consist of alphanumeric character. It may also include an underscore or a dash."
-
-    until [[ $CLIENT =~ ^[a-zA-Z0-9_-]+$ ]]; do
-        read -rp "Client name: " -e CLIENT
-    done
-
-    echo ""
-    echo "Do you want to protect the configuration file with a password?"
-    echo "(e.g. encrypt the private key with a password)"
-    echo "   1) Add a passwordless client"
-    echo "   2) Use a password for the client"
-
-    until [[ $PASS =~ ^[1-2]$ ]]; do
-        read -rp "Select an option [1-2]: " -e -i 1 PASS
-    done
-
-    CLIENTEXISTS=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep -c -E "/CN=$CLIENT\$")
-    if [[ $CLIENTEXISTS == '1' ]]; then
-        echo ""
-        echo "The specified client CN was already found in easy-rsa, please choose another name."
-        exit
-    else
-        cd /etc/openvpn/easy-rsa/ || return
-        case $PASS in
-        1)
-            ./easyrsa build-client-full "$CLIENT" nopass
-            ;;
-        2)
-            echo "⚠️ You will be asked for the client password below ⚠️"
-            ./easyrsa build-client-full "$CLIENT"
-            ;;
-        esac
-        echo "Client $CLIENT added."
-    fi
-
-    # Home directory of the user, where the client configuration will be written
-    if [ -e "/home/${CLIENT}" ]; then
-        # if $1 is a user name
-        homeDir="/home/${CLIENT}"
-    elif [ "${SUDO_USER}" ]; then
-        # if not, use SUDO_USER
-        if [ "${SUDO_USER}" == "root" ]; then
-            # If running sudo as root
-            homeDir="/root"
-        else
-            homeDir="/home/${SUDO_USER}"
-        fi
-    else
-        # if not SUDO_USER, use /root
-        homeDir="/root"
-    fi
-
-    # Determine if we use tls-auth or tls-crypt
-    if grep -qs "^tls-crypt" /etc/openvpn/server.conf; then
-        TLS_SIG="1"
-    elif grep -qs "^tls-auth" /etc/openvpn/server.conf; then
-        TLS_SIG="2"
-    fi
-
-    # Generates the custom client.ovpn
-    cp /etc/openvpn/client-template.txt "$homeDir/$CLIENT.ovpn"
-    {
-        echo "<ca>"
-        cat "/etc/openvpn/easy-rsa/pki/ca.crt"
-        echo "</ca>"
-
-        echo "<cert>"
-        awk '/BEGIN/,/END/' "/etc/openvpn/easy-rsa/pki/issued/$CLIENT.crt"
-        echo "</cert>"
-
-        echo "<key>"
-        cat "/etc/openvpn/easy-rsa/pki/private/$CLIENT.key"
-        echo "</key>"
-
-        case $TLS_SIG in
-        1)
-            echo "<tls-crypt>"
-            cat /etc/openvpn/tls-crypt.key
-            echo "</tls-crypt>"
-            ;;
-        2)
-            echo "key-direction 1"
-            echo "<tls-auth>"
-            cat /etc/openvpn/tls-auth.key
-            echo "</tls-auth>"
-            ;;
-        esac
-    } >>"$homeDir/$CLIENT.ovpn"
-
-    echo ""
-    echo "The configuration file has been written to $homeDir/$CLIENT.ovpn."
-    echo "Download the .ovpn file and import it in your OpenVPN client."
-
-    exit 0
-}
 """
 
-def revokeClient():
+def load_pki_index():
     pki_index_path = "/etc/openvpn/easy-rsa/pki/index.txt"
-    NUMBEROFCLIENTS = 0
     CLIENTS=[]
     with open(pki_index_path, mode='r') as os_release:
         lines = os_release.read()
@@ -1191,8 +1092,104 @@ def revokeClient():
             if idx == 1:
                 continue
             if line.startswith("V"):
-                NUMBEROFCLIENTS += 1
                 CLIENTS.append(line.split('=')[1].strip())
+    return CLIENTS
+
+def newClient():
+    print("")
+    print("Tell me a name for the client.")
+    print("The name must consist of alphanumeric character. It may also include an underscore or a dash.")
+
+    CLIENT = ''
+    while not re.match(r'^[a-zA-Z0-9_-]+$', CLIENT):
+        print("Client name: ", end='')
+        CLIENT = input()
+
+    print("")
+    print("Do you want to protect the configuration file with a password?")
+    print("(e.g. encrypt the private key with a password)")
+    print("   1) Add a passwordless client")
+    print("   2) Use a password for the client")
+    PASS = ''
+    while PASS not in ('1','2'):
+        print("Select an option [1-2]: ", end='')
+        PASS = input()[0]
+
+    CLIENTEXISTS = load_pki_index()
+    if CLIENT in CLIENTEXISTS:
+        print("")
+        print("The specified client CN was already found in easy-rsa, please choose another name.")
+        sys.exit(-1)
+    else:
+        os.chdir("/etc/openvpn/easy-rsa/")
+        if PASS == '1':
+            os.system('./easyrsa build-client-full "' + CLIENT + '" nopass')
+        else:
+            print("You will be asked for the client password below")
+            os.system('./easyrsa build-client-full "' + CLIENT + '"')
+        print("Client " + CLIENT + " added.")
+
+    # # Home directory of the user, where the client configuration will be written
+    # if [ -e "/home/${CLIENT}" ]; then
+    #     # if $1 is a user name
+    #     homeDir="/home/${CLIENT}"
+    # elif [ "${SUDO_USER}" ]; then
+    #     # if not, use SUDO_USER
+    #     if [ "${SUDO_USER}" == "root" ]; then
+    #         # If running sudo as root
+    #         homeDir="/root"
+    #     else:
+    #         homeDir="/home/${SUDO_USER}"
+    # else:
+    #     # if not SUDO_USER, use /root
+    #     homeDir="/root"
+    # # Determine if we use tls-auth or tls-crypt
+    # if grep -qs "^tls-crypt" /etc/openvpn/server.conf; then
+    #     TLS_SIG="1"
+    # elif grep -qs "^tls-auth" /etc/openvpn/server.conf; then
+    #     TLS_SIG="2"
+    # fi
+
+    # # Generates the custom client.ovpn
+    # cp /etc/openvpn/client-template.txt "$homeDir/$CLIENT.ovpn"
+    # {
+    #     echo "<ca>"
+    #     cat "/etc/openvpn/easy-rsa/pki/ca.crt"
+    #     echo "</ca>"
+    # 
+    #     echo "<cert>"
+    #     awk '/BEGIN/,/END/' "/etc/openvpn/easy-rsa/pki/issued/$CLIENT.crt"
+    #     echo "</cert>"
+    # 
+    #     echo "<key>"
+    #     cat "/etc/openvpn/easy-rsa/pki/private/$CLIENT.key"
+    #     echo "</key>"
+    # 
+    #     case $TLS_SIG in
+    #     1)
+    #         echo "<tls-crypt>"
+    #         cat /etc/openvpn/tls-crypt.key
+    #         echo "</tls-crypt>"
+    #         ;;
+    #     2)
+    #         echo "key-direction 1"
+    #         echo "<tls-auth>"
+    #         cat /etc/openvpn/tls-auth.key
+    #         echo "</tls-auth>"
+    #         ;;
+    #     esac
+    # } >>"$homeDir/$CLIENT.ovpn"
+    print("")
+    print("The configuration file has been written to $homeDir/$CLIENT.ovpn.")
+    print("Download the .ovpn file and import it in your OpenVPN client.")
+
+    sys.exit(0)
+
+def revokeClient():
+    pki_index_path = "/etc/openvpn/easy-rsa/pki/index.txt"
+    
+    CLIENTS = load_pki_index()
+    NUMBEROFCLIENTS = len(CLIENTS)
     if NUMBEROFCLIENTS == 0:
         print("")
         print("You have no existing clients!")
@@ -1220,6 +1217,7 @@ def revokeClient():
     os.chdir(CURRENT_PATH)
     os.remove(CLIENT + ".ovpn")
     # TODO
+    print("TODO do with something /etc/openvpn/ipp.txt")
     # sed -i "/^$CLIENT,.*/d" /etc/openvpn/ipp.txt
     print("")
     print("Certificate for client " + CLIENT + " revoked.")

4e8e6a960b023f84df6094226aabb720b479a23b
Evgenii Sopov <mrseakg@gmail.com>
2023-10-26 23:13:33 +0700

    Improve test_send_flags.py

--- tests/test_send_flags.py
+++ tests/test_send_flags.py
@@ -4,16 +4,21 @@
 import requests
 import time
 import uuid
+import random
 
 host = "localhost"
 port = 8080
-teamnum = 1
+teamid = "t01"
 
 i = 0
-while i < 1000:
+while i < 1000000000:
     i = i + 1
+    teamid = random.randint(1, 20)
+    teamid = 't' + str(teamid).rjust(2, '0')
+
+    print(teamid)
     flag = str(uuid.uuid4())
-    r = requests.get('http://' + host + ':' + str(port) + '/flag?teamid=some&flag=' + flag)
+    r = requests.get('http://' + host + ':' + str(port) + '/flag?teamid=' + str(teamid) + '&flag=' + flag)
     if r.status_code == 200:
         print("OK")
     elif r.status_code == 403:
